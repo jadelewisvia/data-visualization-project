e920761fcd6ebfc8431d3005747399214d3c04bd
jadelewisvia <jadel55@nycstudents.net>
2025-12-16 15:27:22 +0000

    hiii

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const ctx = document.getElementById('myChart')
 new Chart(ctx, {
     type:'bar',
     data: {
+        labels: ['A', 'B', 'C', 'D', 'E']
+        datasets: [{
+            label: 'Example Data'
+            data: 
