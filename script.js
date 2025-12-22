3931b09888ab7ce7f9e55923729561e1828f6014
jadelewisvia <jadel55@nycstudents.net>
2025-12-22 15:29:13 +0000

    aaa

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ new Chart(ctx, {
         labels: ['A', 'B', 'C', 'D', 'E']
         datasets: [{
             label: 'Example Data'
-            data: 
+            data: {12, 19, 3, 5, 2}
+            borderWidth: 1,
+            backgroundColor: ['#ff6384', '#36a2eb', '#ffcd56', '#4bc0c0',]
+          }]
+        },
+        options: {
+            scales: {
+                y: { beginAtZero: true }
+            }
